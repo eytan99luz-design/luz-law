124d4f5ed36847ba34251e22d7f28d939efe2dfc
eytan99luz-design <eytan99luz@gmail.com>
2026-04-09 20:47:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: /luz-law/", //
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
